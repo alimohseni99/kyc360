2b97c54520bd8bfd289d16dbcfb766183fb90b4d
Ali <ali.mohseni05@yahoo.se>
2024-11-21 22:06:49 +0100

    implemented instance.ts in account-management

--- src/features/account-management/schema/schema.ts
+++ src/features/account-management/schema/schema.ts
@@ -6,7 +6,6 @@ export const accountTable = pgTable("account", {
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   account_type: varchar({ length: 50 }).notNull(),
-  role: varchar({ length: 255 }).notNull(),
   created_at: timestamp()
     .notNull()
     .default(sql`now()`),
