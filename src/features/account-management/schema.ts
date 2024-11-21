fa78715de3823dc345cdf63864b7babb29083554
Ali <ali.mohseni05@yahoo.se>
2024-11-21 21:46:52 +0100

    implemented account-management schemat.ts and its table

---
+++ src/features/account-management/schema.ts
@@ -0,0 +1,13 @@
+import { sql } from "drizzle-orm";
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const accountTable = pgTable("account", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  role: varchar({ length: 255 }).notNull(),
+  created_at: timestamp()
+    .notNull()
+    .default(sql`now()`),
+});
