9457b0a40f424e540c8a231f2826db7d07c9e939
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-27 17:11:11 +0100

    added popover for cards

--- src/features/account-management/components/get-all-card/get-all-card.tsx
+++ src/features/account-management/components/get-all-card/get-all-card.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,7 +7,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Header from "../dashboard/header";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { DeleteIcon, EditIcon, EyeIcon } from "lucide-react";
 
 // Färger för statusindikatorn
 const statusColors: Record<string, string> = {
@@ -26,25 +31,52 @@ type Props = {
 
 export function GetAllCard({ id, name, email, status }: Props) {
   return (
-    <>
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-lg font-bold">Name: {name}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-sm text-gray-700">Email: {email}</p>
-        </CardContent>
-        <CardFooter className="flex justify-end">
-          <div className="flex items-center mt-4">
-            <span
-              className={`h-3 w-3 rounded-full ${statusColors[status]} mr-2`}
-            ></span>
-            <p className="text-sm font-medium capitalize bg-">
-              Status: {status}
-            </p>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle className="text-lg font-bold">
+          <div className="flex items-center flex-wrap justify-between">
+            <span>{name}</span>
+            <div className="flex-shrink-0">
+              <Popover>
+                <PopoverTrigger className="">...</PopoverTrigger>
+                <PopoverContent>
+                  <div className="flex flex-col gap-3">
+                    <Button
+                      variant="ghost"
+                      className="flex items-center max-w-sm"
+                    >
+                      <EyeIcon className="mr-2" /> View
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      className="flex items-center w-full"
+                    >
+                      <EditIcon className="mr-2" /> Edit
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      className="flex items-center w-full"
+                    >
+                      <DeleteIcon className="mr-2" /> Delete
+                    </Button>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </div>
           </div>
-        </CardFooter>
-      </Card>
-    </>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm text-gray-700">Email: {email}</p>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <div className="flex items-center mt-4">
+          <span
+            className={`h-3 w-3 rounded-full ${statusColors[status]} mr-2`}
+          ></span>
+          <p className="text-sm font-medium capitalize">Status: {status}</p>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
