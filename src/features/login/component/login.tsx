7c1b4f5e77c2174fa45bb32811786ddcaf415e1f
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-29 11:26:40 +0100

    small admin account change

--- src/features/login/component/login.tsx
+++ src/features/login/component/login.tsx
@@ -17,7 +17,7 @@ import { bankUsersLogin } from "../action";
 
 export function Login() {
   const representativeSchema = z.object({
-    email: z.string().email({ message: "Invalid email address" }),
+    email: z.string({ message: "Invalid email address" }),
     password: z
       .string()
       .min(4, { message: "Password must be at least 4 characters" }),
